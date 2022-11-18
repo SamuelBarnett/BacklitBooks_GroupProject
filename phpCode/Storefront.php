c1a8805b72acbdd8ed592e7e21fcbf249eed7dd8
Samuel Barnett <sambiam888@gmail.com>
2022-11-18 10:56:37 -0500

    fixed css

--- phpCode/Storefront.php
+++ phpCode/Storefront.php
@@ -22,7 +22,7 @@ $rated_books = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="Storefront.css" />
+    <link rel="stylesheet" href="./styles/main.css" />
     <title>Storefront</title>
     <style></style>
 </head>
