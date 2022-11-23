b399f314e6c7320bd71212089c038dcc83a03773
DToddjm <97998704+DToddjm@users.noreply.github.com>
2022-11-22 22:50:50 -0500

    Add files via upload

--- BacklitBooks/DeleteAccount.php
+++ BacklitBooks/DeleteAccount.php
@@ -1,7 +1,12 @@
 <?php 
     session_start();
     include 'DBconnection.php';
+    // delete from orders table first
     $pdo = connectDB();
+    $sql = "DELETE FROM orders WHERE UserID = ?;";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$_SESSION['uID']]);
+    // delete from users table
     $sql = "DELETE FROM users WHERE UserID = ?;";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$_SESSION['uID']]);
